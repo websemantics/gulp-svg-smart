32fe75ad74662606215b9612c01469b3aad8e2d7
Adnan <adnan@websemantics.ca>
2016-04-06 10:47:59 -0400

    new version

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Gulp SVG Smart
+```
+    ______    __________           ____  __  ___  ___     ___    ______
+   / ____ |  /  _  ____/   ____   / __/ /  |/  / / _ |   / _ \  /_  __/
+   \ \  | | / / / / __    /___/  _\ \  / /|_/ / / __ |  / , _/   / /
+  __\ \ | |/ / / (_/ /          /___/ /_/  /_/ /_/ |_| /_/|_|   /_/
+ /____/ |___/  \____/ GULP PLUGIN v0.1.3
+```
+> Updated : April 6, 2016
 
-Gulp wrapper for svg-smart node.js module, plus implementation of common gulp tasks.
+Gulp wrapper and pre-packged Gulp Tasks for svg-smart node.js module.
+
+## Releated
+
+* [SVG SMART](https://github.com/websemantics/svg-smart), a Node.js module that generates all your svg, png, icon and svg sprites needs using a data-driven svg template system.
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /**
- *  GULP PLUGIN for,
  *    ______    __________           ____  __  ___  ___     ___    ______
  *   / ____ |  /  _  ____/   ____   / __/ /  |/  / / _ |   / _ \  /_  __/
  *   \ \  | | / / / / __    /___/  _\ \  / /|_/ / / __ |  / , _/   / /
  *  __\ \ | |/ / / (_/ /          /___/ /_/  /_/ /_/ |_| /_/|_|   /_/
- * /____/ |___/  \____/ v0.1.2
+ * /____/ |___/  \____/ GULP PLUGIN v0.1.3
  *
- * Gulp wrapper for svg-smart node.js module, plus implementation of common gulp tasks.
+ * Gulp wrapper and pre-packged Gulp Tasks for svg-smart node.js module.
  *
  * @link      http://websemantics.ca
  * @author    Web Semantics, Inc. Dev Team <team@websemantics.ca>
@@ -15,6 +14,7 @@
 
  'use strict';
 
+ var gulp           = require("gulp");
  var sprite         = require("gulp-svg-sprite");
  var svg2png        = require("gulp-svg2png");
  var favicons       = require("gulp-favicons");
@@ -26,75 +26,69 @@
 // ---------------------------------------------------
 // TASK : default - Generate preview page, index.html
 // ---------------------------------------------------
-function _default(gulp, resources) {
+gulp.task('svg-smart-tasks', ['svg-smart-sprite'], function() {
   // return gulp.src(resources.html.template)
   //   .pipe(template({ext:"tmp", data: resources}))
   //   .pipe(rename(resources.html.filename))
   //   .pipe(gulp.dest(resources.html.dest));
-  console.log(resources);
-}
+});
 
-//
-//
-// // ---------------------------------------------------
-// // TASK : process SVG spritesc
-// // ---------------------------------------------------
-// gulp.task('sprite', ['icon'], function() {
-//   var stream = gulp;
-//   for (var i in resources.sprite) {
-//       stream = gulp.src(resources.sprite[i].source)
-//           .pipe(sprite(resources.sprite[i].config))
-//           .pipe(gulp.dest(resources.sprite[i].dest));
-//   }
-//   return stream;
-// });
-//
-// // ---------------------------------------------------
-// // TASK : icon
-// // ---------------------------------------------------
-// gulp.task('icon', ['png'], function() {
-//   var stream = gulp;
-//   for (var i in resources.icon) {
-//     stream =  gulp.src(resources.icon[i].source)
-//         .pipe(favicons(resources.icon[i].config))
-//         .on('error', function(){})
-//         .pipe(gulp.dest(resources.icon[i].dest));
-//   }
-//   return stream;
-// });
-//
-// // ---------------------------------------------------
-// // TASK : png
-// // ---------------------------------------------------
-// gulp.task('png', ['svg'], function() {
-//   var stream = gulp;
-//   for (var i in resources.png) {
-//       stream = gulp.src(resources.png[i].source)
-//           .pipe(svg2png([resources.png[i].scale]))
-//           .pipe(rename(resources.png[i].filename + "." + resources.png[i].extension))
-//           .pipe(gulp.dest(resources.png[i].dest));
-//   }
-//   return stream;
-// });
-//
-// // ---------------------------------------------------
-// // TASK : svg, generate svg files
-// // ---------------------------------------------------
-// gulp.task('svg', function() {
-//
-//   resources = smart.load("smart.json", "package.json");
-//
-//   var stream = gulp;
-//   for (var i in resources.svg) {
-//         stream = gulp.src(resources.svg[i].template)
-//           .pipe(template({ext:"tmp", data: resources.svg[i].data}))
-//           .pipe(minify())
-//           .pipe(rename(resources.svg[i].filename + "." + resources.svg[i].extension))
-//           .pipe(gulp.dest(resources.svg[i].dest));
-//   }
-//   return stream;
-// });
-//
-//
+// ---------------------------------------------------
+// TASK : process SVG spritesc
+// ---------------------------------------------------
+gulp.task('svg-smart-sprite', ['svg-smart-icon'], function() {
+  // var stream = gulp;
+  // for (var i in resources.sprite) {
+  //     stream = gulp.src(resources.sprite[i].source)
+  //         .pipe(sprite(resources.sprite[i].config))
+  //         .pipe(gulp.dest(resources.sprite[i].dest));
+  // }
+  // return stream;
+});
 
-module.exports = {"default": _default};
+// ---------------------------------------------------
+// TASK : icon
+// ---------------------------------------------------
+gulp.task('svg-smart-icon', ['svg-smart-png'], function() {
+  // var stream = gulp;
+  // for (var i in resources.icon) {
+  //   stream =  gulp.src(resources.icon[i].source)
+  //       .pipe(favicons(resources.icon[i].config))
+  //       .on('error', function(){})
+  //       .pipe(gulp.dest(resources.icon[i].dest));
+  // }
+  // return stream;
+});
+
+// ---------------------------------------------------
+// TASK : png
+// ---------------------------------------------------
+gulp.task('svg-smart-png', ['svg-smart-svg'], function() {
+  // var stream = gulp;
+  // for (var i in resources.png) {
+  //     stream = gulp.src(resources.png[i].source)
+  //         .pipe(svg2png([resources.png[i].scale]))
+  //         .pipe(rename(resources.png[i].filename + "." + resources.png[i].extension))
+  //         .pipe(gulp.dest(resources.png[i].dest));
+  // }
+  // return stream;
+});
+
+// ---------------------------------------------------
+// TASK : svg, generate svg files
+// ---------------------------------------------------
+gulp.task('svg-smart-svg', function() {
+  // resources = smart.load("smart.json", "package.json");
+  //
+  // var stream = gulp;
+  // for (var i in resources.svg) {
+  //       stream = gulp.src(resources.svg[i].template)
+  //         .pipe(template({ext:"tmp", data: resources.svg[i].data}))
+  //         .pipe(minify())
+  //         .pipe(rename(resources.svg[i].filename + "." + resources.svg[i].extension))
+  //         .pipe(gulp.dest(resources.svg[i].dest));
+  // }
+  // return stream;
+});
+
+module.exports = gulp;
